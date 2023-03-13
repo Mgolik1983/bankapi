521124efd7978380067b0f79558805119a9d7eeb
Mgolik16983 <mgolik1983@gmail.com>
2023-03-13 17:13:56 +0300

    initial

--- Core/schemas/__init__.py
+++ Core/schemas/__init__.py
@@ -1,7 +1,8 @@
-from .user import UserInfo, RegisterForm
+from .user import UserInfo, RegisterForm, LoginForm
 
 
 __all__ = [
     'UserInfo',
-    'RegisterForm'
+    'RegisterForm',
+    'LoginForm'
 ]
