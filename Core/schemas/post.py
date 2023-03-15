2f7e5962f5fbd0a07aa34fe9029fbd4925d54ba5
Mgolik16983 <mgolik1983@gmail.com>
2023-03-15 14:59:10 +0300

    initial

---
+++ Core/schemas/post.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class PostCreateForm(BaseModel):
+    title: str = Field(
+        max_length=128,
+        title='Post title',
+        description='Post Uniue Title'
+    )
+    body: str = Field(
+        title='Post body',
+        description='Post body'
+    )
+class PostDetail(PostCreateForm):
+    pk: int = Field(
+        ge=1,
+        title='Post id',
+        description='Post Uniue Id'
+    )
+    date_created: datetime = Field(
+        title='Post Date Created',
+        description='Post Date Created',
+    )
+    author_id: int = Field(
+        ge=1,
+        title='Post Author ID',
+        description='Post Author Unique ID'
+    )
